d4d3d08c7b4c2e94ecfc9e1f51a13f135727ef36
choscura <choscura@gmail.com>
2016-11-25 18:31:36 -0800

    Adding prototype api.py file, will begin testing motor functions remotely via this, and integrate into django RESTful API.

---
+++ api.py
@@ -0,0 +1,32 @@
+"""	
+
+	fast prototype API to allow basic motor function web hooks and motor feedback. For brevity, this is a single file prototype, intended to be expanded into a proper python import and executable program to test things via LAMP and Django over LAN, with the intention of implementing in production VPN networks with full security protocols needed to meet basic security best practices.
+
+
+Basic structure: 
+
+Initialization: making the turret ready to use. This is an an in-house operation and also a set of self-correcting behaviors.
+
+Motor controls:  This includes two major subsets (at least initially): a manual control mode, where the turret can be aimed by hand, and an automated mode, where the turret can perform specific tasks, such as search patterning, motion tracking, or cycling through a series of known coordinates to take a sequence of motions [aiming a series of photographs or positioning sensors for a sequence of automated measurements et al]
+
+Feedback handling and data passing: This set of functionality includes showing specific sets of relevant data, for example, checking for torque to avoid obstructions to the motor's path of travel.
+"""
+
+
+"""
+INITIALIZATION
+"""
+
+"""
+MAPPING
+"""
+
+
+"""
+MOTORCONTROL
+"""
+
+"""
+DATAFEED
+"""
+
