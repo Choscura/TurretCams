044fd0f2d30b4c87a6fa52661c65b9ff2cbd38ed
choscura <choscura@gmail.com>
2016-11-27 15:23:08 -0800

    updating prototype API file, fleshing out functions and stitching everything together to make the motors do stuff.

--- api.py
+++ api.py
@@ -11,11 +11,64 @@ Motor controls:  This includes two major subsets (at least initially): a manual 
 
 Feedback handling and data passing: This set of functionality includes showing specific sets of relevant data, for example, checking for torque to avoid obstructions to the motor's path of travel.
 """
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+import os
+import time
+import json
+from string import *
+from threading import Thread
 
 
+
+"""try:		#Python 2.7 remaindered versions
+	from dxl import *
+	from dxl.dxlcore import * 
+	from serial import SerialException
+except:		#Python 3.5"""
+
+from pyax12.connection import Connection
+
+conn = Connection(port='/dev/ttyACM0', baudrate=57600, timeout=0.1, waiting_time=0.02)
 """
 INITIALIZATION
+
+	This is an internal process for the Pi controling the motors to run locally and report on: it should take no variables to run any of this by default, and it should allow the pi to initialize itself independently in any environment it has sufficient power to run in.
+
+This initialization is broken into several parts: for the current iteration, the focus is on building a map of the available points of movement. At a future point, the priority will be to correlate a set of known points with a set of expected visual fedbacks (because, theoretically, a solidly mounted camera turret should see more or less the same things from the same camera positions most of the time, within the confines of environmental variables).
+
 """
+
+###### Temp section for learning funcitons #######
+
+print("attempting to connect to and scan motors")
+ids_available = conn.scan()
+print("the scan finished, now it should print something")
+for ids in ids_available:
+	print("there should be a motor ID here")
+	print(ids)
+
+
+def movezero():
+	"""
+	This is a function to move the motors both to the 512 <centerline> position.
+	"""
+	conn.goto(1, 512, speed=0, degrees=False)
+	conn.goto(2, 512, speed=0, degrees=False)	
+
+
+def setup():
+	pass
+	#move both motors to 512 positon (0^ center for both motors)
+
+
+
+
+"""
+
+"""
+
 
 """
 MAPPING
@@ -30,3 +83,12 @@ MOTORCONTROL
 DATAFEED
 """
 
+
+"""
+MAINLOOP
+"""
+
+
+
+########################################################################################################
+##########		code graveyard, for remaindered and old code that is still nonetheless instructive #####
