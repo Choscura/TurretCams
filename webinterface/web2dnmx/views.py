e207db5b27d4e7920871cd5bdf2456ad1bfd1948
Choscura <choscura@gmail.com>
2016-12-02 15:12:50 -0800

    modifying project to containt a virtual environment for granularity and greater version control

--- webinterface/web2dnmx/views.py
+++ webinterface/web2dnmx/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-#from dxl.dxlchain import DxlChain
+from dxl.dxlchain import DxlChain
 
 import datetime
 
@@ -10,7 +10,7 @@ def current_datetime(request):
     #chain=DxlChain("/dev/ttyACM0", rate=57142, timeout=0.1, waiting_time=0.02)
 	#motors = chain.get_motor_list()
 	#try:
-	#		chain.sync_write_pos_speed([1,2][512,512],[512,512])
+	#		chain.sync_write_pos_speed([1,2][512,512],[512,512])"""
 	page="<html><body>It is now %s</body></html>" %now
 	
 	return HttpResponse(page)
