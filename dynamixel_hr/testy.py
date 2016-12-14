f154390abbc63e9b06f8649a5bfbcb4de4d3b160
choscura <choscura@gmail.com>
2016-12-14 11:44:03 -0800

    bring turret up to date and making sure everything is as it should be before modifying as part of diagnostics.

--- dynamixel_hr/testy.py
+++ dynamixel_hr/testy.py
@@ -1,3 +1,7 @@
-import ToolDynamixelLab
+#import ToolDynamixelLab
+from dxl.dxlchain import DxlChain
+
+chain = DxlChain("/dev/ttyACM0", rate = 57600, timeout = 0.2)
+print chain.get_motor_list()
 
 
