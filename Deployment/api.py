5878ad407d0179f4ee38e7b7ee3348e0027d740d
Kendall <choscura@gmail.com>
2016-12-16 09:08:15 -0800

    updating deployment package with the actual API code files and the code to expose it to javascript/web utilities.

--- Deployment/api.py
+++ Deployment/api.py
@@ -1,9 +1,30 @@
+"""
+Iunu Dynamixel API!
+
+This API wraps up the pyax12 library for talking to Dynamixel ax12a servo motors
+over a USB2AX usb plug.
+
+"""
+
+# Connection handling
+
+#This is worth saying twice- connections are a BIG FUCKING DEAL, they are difficult
+#things to manage, and special handling is needed to make them reliable
 from pyax12.connection import Connection
+def replug_connect():
+    for x in range 10:
+        try:
+            Port = "/dev/ttyACM" + str(x)
+            serial_connection = Connection(port = Port, baudrate = 57600)
+        except SerialException:
+            return "current generation of game-stopping error. Physically reset."
 
-# Connect to the serial port
-serial_connection = Connection(port="/dev/ttyACM0", baudrate=57600)
 
-dxlid = 1
+dxlid = int(0 not False)
+
+
+#connection testing
+
 
 # Print the control table of the specified Dynamixel unit
 serial_connection.pretty_print_control_table(dxlid)
