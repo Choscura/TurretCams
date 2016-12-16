e3cf75cc92fb0e782d9b558b277ca9b5ca72376c
choscura <choscura@gmail.com>
2016-12-15 16:59:22 -0800

    removed redundant files, modified sicko.py as part of experiment.
    Currently, sicko.py runs and communicates with motors, while identical copy (sickotest.py) failed to do this initially and then began working when the order of declarations was changed (!?!?), despite this not actually (apparently?) changing the nature of the variables.

--- README.md
+++ README.md
@@ -4,26 +4,12 @@ This is the readme for the Turret Camera project to run on raspberry pi 3 and co
 turret camera mount system for taking automated graphical measurements of production
 greenhouse environments for the Iunu company.
 
-
 OVERVIEW
 
-	physical architecture
-		Nodes
-			local servers
-			raspberry pi 3 b+
-		Networking
-			Unifi Edge Router
-			Unifi AP-LR
-		actuators
-			dynamixel ax12a
-		sensors
-			yoctopuce
-			whitebox labs
-	logical architecture
-		API's
-			camera
-			other sensor
-			actuator
-			digested_data
-			raw_data
-	division of labor
+hardware configuraiton
+	Motor:	dynamixel ax12a servo motors
+	Main board: Raspberry Pi 3 model B
+	Controller: usb2ax
+	Power hub: IL-3PHUB
+
+computer/os/filesystem environmental variables
--- thesicksperiments/sicko.py
+++ thesicksperiments/sicko.py
@@ -1,32 +1,28 @@
+from flask import Flask, jsonify
 import time
+#from random import randint			# 	testing byproduct, not actually used in deployment
 from pyax12.connection import Connection
-
-
-
-
-
+ 
+connection = Connection(port="/dev/ttyACM0", baudrate=1000000, timeout=0.2, waiting_time=0.02)
 
 def connect():
 	"""
 	serial connection handling
 	"""
-	dxl1 = 1
-	dxl2 = 2
-	connection = Connection(port="/dev/ttyACM0", baudrate=1000000, timeout=0.2, waiting_time=0.02)
+	dxl1=1
+	dxl2=2
 	connection.pretty_print_control_table(dxl1)
 	for i in range(3):
-		connection.goto(1, dxl2 ** i)
+		connection.goto(1,512)
+		connection.goto(2, 512)
 		#connection.goto(1, 300)
 		print(connection.ping(2)) #%(x for x in range 100)
-
-
-
 	
 def close_connection():
-	pass
+	connection.close()	
 
 def get_position():
-	pass
+	return connection.get_present_position()	
 
 def Set_relative_position():
 	pass
@@ -34,6 +30,22 @@ def Set_relative_position():
 def set_absolute_positon():
 	pass
 
+def center():
+	connection.goto(1,512)
+	connection.goto(2,512)
 
 
-connect()
+#flask-specific things and API code
+
+close_connection()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	center()
+	return "%s" %motors
+
+if __name__ == '__main__':
+	app.run(debug=True, host="172.25.1.182", port=8000)
+
