9976f7a4d6bb1c83156ecd3206351bbed411ef86
choscura <choscura@gmail.com>
2016-12-16 14:24:27 -0800

    Fleshing out function list in API, running experiments in sicko.py to try and diagose servo connection issues (error messages vary depending on use cases, but it seems like separate instances of the code are blocking each other from using the servos?)

--- Deployment/api.py
+++ Deployment/api.py
@@ -2,7 +2,9 @@
 Iunu Dynamixel API!
 
 This API wraps up the pyax12 library for talking to Dynamixel ax12a servo motors
-over a USB2AX usb plug.
+over a USB2AX usb plug.  Everything in this file is hard-coded to deal with a turret
+that is based on a raspberry pi 3 model B, running 2 dynamixel ax12a servos. Your
+mileage on different systems using different architectures will vary..
 
 """
 
@@ -11,8 +13,11 @@ over a USB2AX usb plug.
 #This is worth saying twice- connections are a BIG FUCKING DEAL, they are difficult
 #things to manage, and special handling is needed to make them reliable
 from pyax12.connection import Connection
+
+serial_connection = Connection(port="/dev/ttyACM0", baudrate = 57600)
+
 def replug_connect():
-    for x in range 10:
+    for x in range(0,10):
         try:
             Port = "/dev/ttyACM" + str(x)
             serial_connection = Connection(port = Port, baudrate = 57600)
@@ -20,14 +25,17 @@ def replug_connect():
             return "current generation of game-stopping error. Physically reset."
 
 
-dxlid = int(0 not False)
+#TODO: error handling on a TypeError where a '0' is being returned as possibly a 'Null' or 'False'.
+# dxlid = int(0 not False)
 
 
-#connection testing
+
+
+
 
 
 # Print the control table of the specified Dynamixel unit
-serial_connection.pretty_print_control_table(dxlid)
+serial_connection.pretty_print_control_table(1)
 
 # Close the serial connection
 serial_connection.close()
--- thesicksperiments/sicko.py
+++ thesicksperiments/sicko.py
@@ -1,25 +1,63 @@
 from flask import Flask, jsonify
 import time
-#from random import randint			# 	testing byproduct, not actually used in deployment
+from random import randint			# 	testing byproduct, not actually used in deployment
 from pyax12.connection import Connection
  
-connection = Connection(port="/dev/ttyACM0", baudrate=1000000, timeout=0.2, waiting_time=0.02)
+connection = Connection(port="/dev/ttyACM0", baudrate=100000, timeout=0.2, waiting_time=0.02)
 
+#connection.goto(1,0)
+
+if connection.ping(1)==False:
+	print("derp")
+
+@app=Flask(__name__)
+@app.route('/connect')
 def connect():
+	#connection=Connection(port="/dev/ttyACM0", baudrate = 100000, timeout = 0.2, waiting_time = 0.02)
 	"""
 	serial connection handling
 	"""
 	dxl1=1
 	dxl2=2
-	connection.pretty_print_control_table(dxl1)
-	for i in range(3):
-		connection.goto(1,512)
-		connection.goto(2, 512)
-		#connection.goto(1, 300)
-		print(connection.ping(2)) #%(x for x in range 100)
-	
+	cccombo = ""
+	while cccombo == "" or None or Null or 0:
+		try:
+			connection.close()
+			connection = Connection(port="/dev/ttyACM0", baudrate = 57600, timeout = 0.2, waiting_time= 0.02)
+			cccombo+=(str(connection.pretty_print_control_table(dxl1) + str(connection.pretty_print_control_table(dxl2))))
+		except:
+			pass
+	return cccombo
+	#	connection.goto(1,512)
+	#	connection.goto(2, 512)
+	#	#connection.goto(1, 300)
+	#	print(connection.ping(2)) #%(x for x in range 100)
+	#return cccombo
+
+#app = Flask(__name__)
+
+@app.route('/close')	
 def close_connection():
+	constatus = str(connection.ping(1))
 	connection.close()	
+	terr = "motor1 con status"
+	return jsonify(terr = constatus)
+
+
+@app.route('/gorand')
+def gorand():
+	derp=randint(0,1023)
+	connection.goto(1, derp)
+	strong = str(connection.ping(1))
+	derp = randint(0,1023)
+	connection.goto(2, derp)
+	strung = str(connection.ping(2))
+	return "<html><head></head><body><h>" + strong + strung + "</h></body></html>"
+
+
+def motors():
+	r_string = str(connection.ping(1)) + str(connection.ping(2))
+	return r_string
 
 def get_position():
 	return connection.get_present_position()	
@@ -37,12 +75,14 @@ def center():
 
 #flask-specific things and API code
 
-close_connection()
+#close_connection()
 
-app = Flask(__name__)
+#app = Flask(__name__)
+
 
 @app.route('/')
 def index():
+	gorand()
 	center()
 	return "%s" %motors
 
